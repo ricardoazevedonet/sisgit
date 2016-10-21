ec901d41d9f52b0c4b5a77c2d976a0b227d13b50
Cintia Kazue Aono <p-caono@tce.sp.gov.br>
2016-10-21 12:30:26 -0200

    teste cintia

---
+++ sisgit-web/src/main/java/br/com/sisgit/util/TestGitUtil.java
@@ -0,0 +1,10 @@
+package br.com.sisgit.util;
+
+public class TestGitUtil {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
